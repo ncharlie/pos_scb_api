b1c188395deff1c7a2d62e7db62fe5893d4b8d72
ncharlie <chanuwat555@hotmail.com>
2024-12-08 17:08:46 +0700

    add tests

--- controllers/main.py
+++ controllers/main.py
@@ -8,15 +8,15 @@ class SCBPaymentIntegration(http.Controller):
     @http.route(['/pos_qr/notification'], type='http', auth='public', methods=['POST'], csrf=False)
     def payment_callback(self, **kw):
         data = request.httprequest.data
-        print(request.httprequest.host)
-        print(request.httprequest.host_url)
-        print(request.httprequest.remote_addr)
-        print(request.httprequest.remote_user)
+        # print(request.httprequest.host)
+        # print(request.httprequest.host_url)
+        # print(request.httprequest.remote_addr)
+        # print(request.httprequest.remote_user)
 
         result = json.loads(data.decode('utf-8'))
 
         if result.get('transactionId'):
-            print("\n\n SCB: Callback: data", data)
+            # print("\n\n SCB: Callback: data", data)
             request.env["bus.bus"]._sendone(
                 f"scb_payment_callback", "PAYMENT_CALLBACK", data
             )
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,2 @@
-from . import test_qr
+from . import common
+from . import test_controller
